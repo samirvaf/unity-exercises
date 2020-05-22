fb551679df3c65a913a178da820cab585542ce05
Samir Ferreira <samir.ferreira@wildlifestudios.com>
2020-05-22 03:19:03 -0300

    Finish course assignment 2

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{    
+    /// <summary>
+    /// Exercise 9 solution 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates using custom classes
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and tell the deck to print itself
+            Deck deck = new Deck();
+            deck.Print();
+
+            // tell the deck to shuffle and print itself
+            deck.Shuffle();
+            deck.Print();
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            Card topCard = deck.TakeTopCard();
+            Console.WriteLine("{0} of {1}", topCard.Rank, topCard.Suit);
+
+            // take the top card from the deck and print the card rank and suit
+            topCard = deck.TakeTopCard();
+            Console.WriteLine("{0} of {1}", topCard.Rank, topCard.Suit);
+        }
+    }
+}
---
+++ ProgrammingAssignment2/ProgrammingAssignment2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// Programming Assignment 2 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Deals 2 cards to 3 players and displays cards for players
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // print welcome message
+            Console.WriteLine("Hello! This is 'Nothing Like Blackjack', hope you like it!\n");
+            // create and shuffle a deck
+            Deck deck = new Deck();
+            deck.Shuffle();
+            Console.WriteLine("Shuffle deck!\n");
+
+            // deal 2 cards each to 3 players (deal properly, dealing
+            // the first card to each player before dealing the
+            // second card to each player)
+            Card p1Card1 = deck.TakeTopCard();
+            Card p2Card1 = deck.TakeTopCard();
+            Card p3Card1 = deck.TakeTopCard();
+            Card p1Card2 = deck.TakeTopCard();
+            Card p2Card2 = deck.TakeTopCard();
+            Card p3Card2 = deck.TakeTopCard();
+            Card[] allGivenCards = { p1Card1, p2Card1, p3Card1, p1Card2, p2Card2, p3Card2 };
+
+            // flip all the cards over
+            foreach (Card card in allGivenCards)
+            {
+                card.FlipOver();
+                Console.WriteLine($"Flipping over {card.Rank} of {card.Suit}");
+            }
+            Console.WriteLine();
+
+            // print the cards for player 1
+            Console.WriteLine($"Player 1 cards: {p1Card1.Rank} of {p1Card1.Suit} and {p1Card2.Rank} of {p1Card2.Suit} \n");
+
+            // print the cards for player 2
+            Console.WriteLine($"Player 2 cards: {p2Card1.Rank} of {p2Card1.Suit} and {p2Card2.Rank} of {p2Card2.Suit} \n");
+
+            // print the cards for player 3
+            Console.WriteLine($"Player 3 cards: {p3Card1.Rank} of {p3Card1.Suit} and {p3Card2.Rank} of {p3Card2.Suit} \n");
+        }
+    }
+}
