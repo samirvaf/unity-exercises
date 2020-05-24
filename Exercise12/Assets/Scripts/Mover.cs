07c7d918be492d13de77945f5a4d37cb9c944dae
Samir Ferreira <samir.ferreira@wildlifestudios.com>
2020-05-24 20:10:29 -0300

    Add physics 2D to game objects

---
+++ Exercise12/Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves a game object
+/// </summary>
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 1;
+    }
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().
+            AddForce(new Vector2(Random.Range(-500, 500),
+                     Random.Range(-550, 500)));
+    }
+}
