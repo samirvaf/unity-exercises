4ccda2d5be98bc118815c295ec0d627403340eca
Samir Ferreira <samir.ferreira@wildlifestudios.com>
2020-05-24 22:35:35 -0300

    Resize and move game objects

---
+++ Exercise15/Exercise15/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exercise15
+{
+    /// <summary>
+    /// Practice if and switch statements
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********\nGame Menu:\n1 - New Game\n2 - Load Game\n3 - Options\n4 - Quit\n**********");
+            int option = int.Parse(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Creating game ...");
+                    break;
+                case 2:
+                    Console.WriteLine("Loading game ...");
+                    break;
+                case 3:
+                    Console.WriteLine("Opening game options ...");
+                    break;
+                case 4:
+                    Console.WriteLine("Quitting game ...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input!");
+                    break;
+            }
+
+            if (option == 1)
+            {
+                Console.WriteLine("Creating game ...");
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine("Loading game ...");
+            }
+            else if (option == 3)
+            {
+                Console.WriteLine("Opening game options ...");
+            }
+            else if (option == 4)
+            {
+                Console.WriteLine("Quitting game ...");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
---
+++ Exercise16/Assets/Scripts/Resizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Resizes the game object
+/// </summary>
+public class Resizer : MonoBehaviour
+{
+    // Timer support
+    const float TotalResizeSeconds = 3;
+    float elapsedResizeSeconds = 0;
+
+    // Resizing control
+    const float ScaleFactorPerSecond = 1;
+    int scaleFactorSignMultiplier = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedResizeSeconds += Time.deltaTime;
+        if (elapsedResizeSeconds >= TotalResizeSeconds)
+        {
+            Vector2 newScale = transform.localScale;
+            newScale.x += ScaleFactorPerSecond * scaleFactorSignMultiplier;
+            newScale.y += ScaleFactorPerSecond * scaleFactorSignMultiplier;
+            transform.localScale = newScale;
+            scaleFactorSignMultiplier *= -1;
+            elapsedResizeSeconds = 0;
+        }
+    }
+}
