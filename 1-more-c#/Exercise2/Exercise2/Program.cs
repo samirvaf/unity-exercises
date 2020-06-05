d9d88daef3d354d63f72505aea2059ffa666f085
Samir Ferreira <samir.ferreira@wildlifestudios.com>
2020-06-04 22:38:07 -0300

    Add Exercise2 from intermediate course

---
+++ 1-more-c#/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleCards;
+
+namespace Exercise2
+{
+    /// <summary>
+    /// Exercise 2 Solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Practice using lists
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create deck and hand
+            Deck deck = new Deck();
+            List<Card> cards = new List<Card>();
+
+            deck.Shuffle();
+
+            // add card, flip over, and print
+            cards.Add(deck.TakeTopCard());
+            cards[0].FlipOver();
+            cards[0].Print();
+            Console.WriteLine();
+
+            // add another card, flip over, and print both cards
+            cards.Add(deck.TakeTopCard());
+            cards[1].FlipOver();
+            cards[1].Print();
+            cards[0].Print();
+
+            Console.WriteLine();
+        }
+    }
+}
+
