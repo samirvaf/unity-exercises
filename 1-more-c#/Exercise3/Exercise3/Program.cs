f94bfb608e9a9b0d3f85ec71163042c86d7a40eb
Samir Ferreira <samir.ferreira@wildlifestudios.com>
2020-06-08 20:46:01 -0300

    Loops exercises

---
+++ 1-more-c#/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of ints
+            List<int> lista = new List<int>();
+
+            // Populate the list with numbers from 1 to 10
+            for(int i = 1; i <= 10; i++)
+            {
+                lista.Add(i);
+            }
+
+            // Print list elements
+            for(int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            // Remove even numbers from the list
+            for(int i = lista.Count - 1; i >= 0; i--)
+            {
+                if(lista[i] % 2 == 0)
+                {
+                    lista.Remove(lista[i]);
+                }
+            }
+
+            // Print list elements
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+        }
+    }
+}
---
+++ 1-more-c#/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleCards;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Receive 2 numbers and print from the lowest to the highest
+            Console.Write("Type a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Type another number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int bigger = num1;
+            int lower = num2;
+            if (num2 > num1)
+            {
+                bigger = num2;
+                lower = num1;
+            }
+
+            for (int i = lower; lower <= bigger; lower++)
+            {
+                Console.WriteLine(lower);
+            }
+
+            // Instantiate deck class
+            Deck deck = new Deck();
+            List<Card> hand = new List<Card>();
+
+            // Shuffle deck and deal 5 cards
+            deck.Shuffle();
+            for (int i = 0; i < 5; i++)
+            {
+                hand.Add(deck.TakeTopCard());
+            }
+
+            // Flips all cards in hand
+            for (int i = 0; i < hand.Count; i++)
+            {
+                hand[i].FlipOver();
+            }
+
+            // Prints cards in hand
+            foreach (Card card in hand)
+            {
+                card.Print();
+            }
+        }
+    }
+}
