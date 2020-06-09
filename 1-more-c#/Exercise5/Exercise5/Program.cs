16dc921ab1e38d6168c230b29c43fdba6398d50c
Samir Ferreira <samir.ferreira@wildlifestudios.com>
2020-06-08 21:23:47 -0300

    While loop exercise

---
+++ 1-more-c#/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String menu = "*****\n1 - New Game\n2 - Load Game\n3 - Options\n4 - Quit\n*****";
+            Console.WriteLine(menu + "\nSelect a option from the menu");
+            int option = int.Parse(Console.ReadLine());
+
+            while (option != 4)
+            {
+                switch (option) {
+                    case 1:
+                        Console.WriteLine("Creating new game ...\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("Loading game ...\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("Opening options ...\n");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, try again\n");
+                        break;
+                }
+                Console.WriteLine(menu + "\nSelect a option from the menu");
+                option = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Ending game ...");
+        }
+    }
+}
