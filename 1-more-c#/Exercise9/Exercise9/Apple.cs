d7dbe168c3bd6dc2f0b4b0e9a4b101dec484dd66
Samir Ferreira <samir.ferreira@wildlifestudios.com>
2020-06-15 00:24:31 -0300

    Improve solution

---
+++ 1-more-c#/Exercise9/Exercise9/Apple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise9
+{
+    public class Apple
+    {
+        float amountLeft;
+        bool organic;
+
+        public Apple(bool organic, float size)
+        {
+            amountLeft = size;
+            this.organic = organic;
+        }
+
+        public float AmountLeft { get { return amountLeft; } }
+        public bool Organic { get { return organic;  } }
+
+        public void TakeBite(float size)
+        {
+            if (organic)
+            {
+                amountLeft -= size;
+                Console.WriteLine($"Amount left: {amountLeft}");
+            }
+            else
+            {
+                Console.WriteLine("This apple is not organic!");
+            }
+        }
+    }
+}
---
+++ 1-more-c#/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple good = new Apple(true, 10);
+
+            Console.WriteLine(good.AmountLeft);
+            Console.WriteLine(good.Organic);
+        }
+    }
+}
