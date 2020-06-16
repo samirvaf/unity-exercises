9d6b3312a2ff24a15d8520d4a41bef9d7e157d63
Samir Ferreira <samir.ferreira@wildlifestudios.com>
2020-06-15 22:33:30 -0300

    Spawning asteroids ftw

---
+++ 1-more-c#/ProgrammingAssignment2/Assets/scripts/Asteroid.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An asteroid
+/// </summary>
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    Sprite asteroidSprite0;
+    [SerializeField]
+    Sprite asteroidSprite1;
+    [SerializeField]
+    Sprite asteroidSprite2;
+
+
+    public void Initialize(Direction direction, Vector3 location)
+    {
+        float angle = Random.Range(0, 31);
+        switch (direction)
+        {
+            case Direction.Up:
+            {
+                angle += 75;
+                break;
+            }
+            case Direction.Down:
+            {
+                angle += 255;
+                break;
+            }
+            case Direction.Left:
+            {
+                angle += 165;
+                break;
+            }
+            case Direction.Right:
+            {
+                angle += 345;
+                break;
+            }
+        }
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 3f;
+        const float MaxImpulseForce = 5f;
+
+        angle *= Mathf.Deg2Rad;
+        Vector2 moveDirection = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            moveDirection * magnitude,
+            ForceMode2D.Impulse);
+        transform.position = location;
+    }
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+	{
+        // set random sprite for asteroid
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        int spriteNumber = Random.Range(0, 3);
+        if (spriteNumber < 1)
+        {
+            spriteRenderer.sprite = asteroidSprite0;
+        }
+        else if (spriteNumber < 2)
+        {
+            spriteRenderer.sprite = asteroidSprite1;
+        }
+        else
+        {
+            spriteRenderer.sprite = asteroidSprite2;
+        }
+	}
+}
---
+++ 1-more-c#/ProgrammingAssignment2/Assets/scripts/AsteroidSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabAsteroid;
+
+    float asteroidRadius;
+    Asteroid direction;
+    Vector3 location;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Store radius value
+        GameObject asteroid = Instantiate(prefabAsteroid) as GameObject;
+        asteroidRadius = asteroid.GetComponent<CircleCollider2D>().radius;
+        Destroy(asteroid);
+
+        // Spawn asteroid at the right side
+        asteroid = Instantiate(prefabAsteroid) as GameObject;
+        direction = asteroid.GetComponent<Asteroid>();
+        location = asteroid.transform.position;
+        location.y = 0;
+        location.x = asteroidRadius + ScreenUtils.ScreenRight;
+        direction.Initialize(Direction.Left, location);
+
+        // Spawn asteroid at the left side
+        asteroid = Instantiate(prefabAsteroid) as GameObject;
+        direction = asteroid.GetComponent<Asteroid>();
+        location = asteroid.transform.position;
+        location.y = 0;
+        location.x = asteroidRadius + ScreenUtils.ScreenLeft;
+        direction.Initialize(Direction.Right, location);
+
+        // Spawn asteroid at the top
+        asteroid = Instantiate(prefabAsteroid) as GameObject;
+        direction = asteroid.GetComponent<Asteroid>();
+        location = asteroid.transform.position;
+        location.y = asteroidRadius + ScreenUtils.ScreenTop;
+        location.x = 0;
+        direction.Initialize(Direction.Down, location);
+
+        // Spawn asteroid at the bottom
+        asteroid = Instantiate(prefabAsteroid) as GameObject;
+        direction = asteroid.GetComponent<Asteroid>();
+        location = asteroid.transform.position;
+        location.y = asteroidRadius + ScreenUtils.ScreenBottom;
+        location.x = 0;
+        direction.Initialize(Direction.Up, location);
+    }
+}
